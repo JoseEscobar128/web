83f21f710b0ec664d715d2e82cd4f869dfb863c0
DanRFUTT <19030204@uttcampus.edu.mx>
2025-08-17 13:19:41 -0600

    Cambios en componente Profile

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array<int, string>|string|null
+     */
+    // ✅ Aquí está la configuración clave.
+    // Le decimos a Laravel que confíe en el proxy que le envía la petición.
+    protected $proxies = '*';
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
+}
